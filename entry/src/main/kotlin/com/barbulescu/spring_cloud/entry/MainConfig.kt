a29c8bc5fceec1076a60692bfe163fa4dafd0f37
Marius Barbulescu <marius.barbulescu@gmail.com>
2020-12-27 11:53:38 +0100

    add request header to span

---
+++ entry/src/main/kotlin/com/barbulescu/spring_cloud/entry/MainConfig.kt
@@ -0,0 +1,21 @@
+package com.barbulescu.spring_cloud.entry
+
+import org.springframework.cloud.sleuth.http.HttpRequestParser
+import org.springframework.cloud.sleuth.instrument.web.HttpServerRequestParser
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.servlet.http.HttpServletRequest
+
+@Configuration
+class MainConfig {
+
+    @Bean(name = [HttpServerRequestParser.NAME])
+    fun myHttpRequestParser(): HttpRequestParser? {
+        return HttpRequestParser { request, _, span ->
+            val req: Any = request.unwrap()
+            if (req is HttpServletRequest) {
+                span.tag("special-key", req.getHeader("special-key"))
+            }
+        }
+    }
+}

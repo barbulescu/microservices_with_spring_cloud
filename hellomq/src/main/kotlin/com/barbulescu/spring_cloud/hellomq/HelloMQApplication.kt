b2d03441c1a0d2305465e1e2f340812ed527319b
Marius Barbulescu <marius.barbulescu@gmail.com>
2021-01-03 21:33:06 +0100

    add custom span

--- hellomq/src/main/kotlin/com/barbulescu/spring_cloud/hellomq/HelloMQApplication.kt
+++ hellomq/src/main/kotlin/com/barbulescu/spring_cloud/hellomq/HelloMQApplication.kt
@@ -2,25 +2,40 @@ package com.barbulescu.spring_cloud.hellomq
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.sleuth.annotation.NewSpan
+import org.springframework.cloud.sleuth.brave.bridge.BraveTracer
 import org.springframework.jms.annotation.EnableJms
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.jms.core.JmsTemplate
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 @EnableJms
-class HellomqApplication(private val jmsTemplate: JmsTemplate) {
+class HelloMQApplication(val jmsTemplate: JmsTemplate, val processor: SpecialProcessor) {
 
     @JmsListener(destination = "hello-request")
     fun sayHello(name: String) {
         println("Saying: $name")
-    	jmsTemplate.send("hello-response") {
+
+        processor.processSomething()
+
+        jmsTemplate.send("hello-response") {
     		it.createTextMessage("Hello $name!")
 		}
     }
 
 }
 
+@Component
+class SpecialProcessor {
+    @NewSpan("custom_span")
+    fun processSomething() {
+        println("processing something")
+    }
+
+}
+
 fun main(args: Array<String>) {
-    runApplication<HellomqApplication>(*args)
+    runApplication<HelloMQApplication>(*args)
 }
 

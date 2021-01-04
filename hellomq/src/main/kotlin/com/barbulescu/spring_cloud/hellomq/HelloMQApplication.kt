fcfbec325622b39e2b85cb3cfc7745d29c2ea8d2
Marius Barbulescu <marius.barbulescu@gmail.com>
2021-01-04 22:48:49 +0100

    replace println with logger

--- hellomq/src/main/kotlin/com/barbulescu/spring_cloud/hellomq/HelloMQApplication.kt
+++ hellomq/src/main/kotlin/com/barbulescu/spring_cloud/hellomq/HelloMQApplication.kt
@@ -1,9 +1,9 @@
 package com.barbulescu.spring_cloud.hellomq
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.sleuth.annotation.NewSpan
-import org.springframework.cloud.sleuth.brave.bridge.BraveTracer
 import org.springframework.jms.annotation.EnableJms
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.jms.core.JmsTemplate
@@ -12,25 +12,28 @@ import org.springframework.stereotype.Component
 @SpringBootApplication
 @EnableJms
 class HelloMQApplication(val jmsTemplate: JmsTemplate, val processor: SpecialProcessor) {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @JmsListener(destination = "hello-request")
     fun sayHello(name: String) {
-        println("Saying: $name")
+        logger.info("Saying: $name")
 
         processor.processSomething()
 
         jmsTemplate.send("hello-response") {
-    		it.createTextMessage("Hello $name!")
-		}
+            it.createTextMessage("Hello $name!")
+        }
     }
 
 }
 
 @Component
 class SpecialProcessor {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @NewSpan("custom_span")
     fun processSomething() {
-        println("processing something")
+        logger.info("processing something")
     }
 
 }
